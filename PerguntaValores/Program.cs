813d9220c322d915fabbe5d7c9fd91d48624d404
ynnk <david.f.mendes@proton.me>
2023-03-08 14:35:59 +0000

    Added Volume calculator

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -6,13 +6,15 @@ namespace PerguntaValores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the area of the cylinder:");
-            int area = Convert.ToInt32(Console.ReadLine());
-
             Console.WriteLine("Enter the radius of the cylinder:");
+            int radius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the height of the cylinder:");
             int height = Convert.ToInt32(Console.ReadLine());
             
-
+            float Pi = 3.1415926f; 
+            double Volume = Pi * Math.Pow(radius, radius) * height;
+            Console.WriteLine($"The volume of the cylinder is {Volume}");
 
 
         }
