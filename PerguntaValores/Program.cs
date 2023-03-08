031ae9afecbd90e05fb5fd1f5a9c301ea6647d02
ynnk <david.f.mendes@proton.me>
2023-03-08 14:11:28 +0000

    Added cylinder area and height user input

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the area of the cylinder:");
+            int area = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the height of the cylinder:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            
+        }
+    }
+}
