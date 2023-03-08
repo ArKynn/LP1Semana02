efe4f46cb41c07f5ca38f8306111f0a5a7bbd5d0
ynnk <david.f.mendes@proton.me>
2023-03-08 14:59:51 +0000

    Added prints for each var type max and min value

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -6,7 +6,67 @@ namespace Specials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            sbyte maxsbyte = sbyte.MaxValue;
+            sbyte minsbyte = sbyte.MinValue;
+
+            Console.WriteLine($"Max possible sbyte value: {maxsbyte}");
+            Console.WriteLine($"Min possible sbyte value: {minsbyte}");
+
+            byte maxbyte = byte.MaxValue;
+            byte minbyte = byte.MinValue;
+
+            Console.WriteLine($"Max possible byte value: {maxbyte}");
+            Console.WriteLine($"Min possible byte value: {minbyte}");
+
+            short maxshort = short.MaxValue;
+            short minshort = short.MinValue;
+
+            Console.WriteLine($"Max possible short value: {maxshort}");
+            Console.WriteLine($"Min possible short value: {minshort}");
+
+            ushort maxushort = ushort.MaxValue;
+            ushort minushort = ushort.MinValue;
+
+            Console.WriteLine($"Max possible ushort value: {maxushort}");
+            Console.WriteLine($"Min possible ushort value: {minushort}");
+
+            int maxint = int.MaxValue;
+            int minint = int.MinValue;
+
+            Console.WriteLine($"Max possible int value: {maxint}");
+            Console.WriteLine($"Min possible int value: {minint}");
+
+            uint maxuint = uint.MaxValue;
+            uint minuint = uint.MinValue;
+
+            Console.WriteLine($"Max possible uint value: {maxuint}");
+            Console.WriteLine($"Min possible uint value: {minuint}");
+
+            long maxlong = long.MaxValue;
+            long minlong = long.MinValue;
+
+            Console.WriteLine($"Max possible long value: {maxlong}");
+            Console.WriteLine($"Min possible long value: {minlong}");
+
+            ulong maxulong = ulong.MaxValue;
+            ulong minulong = ulong.MinValue;
+
+            Console.WriteLine($"Max possible ulong value: {maxulong}");
+            Console.WriteLine($"Min possible ulong value: {minulong}");
+
+            float maxfloat = float.MaxValue;
+            float minfloat = float.MinValue;
+
+            Console.WriteLine($"Max possible float value: {maxfloat}");
+            Console.WriteLine($"Min possible float value: {minfloat}");
+
+            double maxdouble = double.MaxValue;
+            double mindouble = double.MinValue;
+
+            Console.WriteLine($"Max possible double value: {maxdouble}");
+            Console.WriteLine($"Min possible double value: {mindouble}");
+
+
         }
     }
 }
