8152eba4791084a9dd92ff117fc8c448077965bd
ynnk <david.f.mendes@proton.me>
2023-03-10 14:58:04 +0000

    Creates triangle based on args given (size, symbol)

---
+++ Triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string simbol = "";
+            int size = Convert.ToInt16(args[0]);
+            
+            for (int i = 0; i < size ; i++)
+            {
+                simbol += args[1];
+                
+                Console.WriteLine(simbol);
+            }
+
+        }
+    }
+}
